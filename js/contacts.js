94758ebfd5e9c063d337d0bcfc1d4953d16dbc23
Tobias Roeske <122990092+tobiasroeske@users.noreply.github.com>
2024-02-19 17:16:21 +0100

    contact page almost done

--- js/contacts.js
+++ js/contacts.js
@@ -3,52 +3,75 @@ let contacts = [
         name: 'max mustermann',
         email: 'max.mustermann@gmail.com',
         phone: '+49 157789562',
-        color: 'red'
+        color: 'red',
+        id: 0
     },
     {
         name: 'Tobias Roeske',
         email: 'roeske.tobias@gmail.com',
         phone: '+49 155589562',
-        color: 'green'
+        color: 'green',
+        id: 1
     },
     {
         name: 'Bünyamin Fajrer',
         email: 'fajrer@gmx.de',
         phone: '+49 1577895562',
-        color: 'darkblue'
+        color: 'darkblue',
+        id: 2
+    },
+    {   name: 'Benedikt Wittmann',
+        email: 'witti.b@web.de',
+        phone: '+49163587654',
+        color: 'yellow',
+        id: 3
     },
     {
         name: 'Erekle Toedten',
         email: 'erekleantidze@gmail.com',
         phone: '+49 157789562',
-        color: 'purple'
+        color: 'purple',
+        id: 4
     },
 ];
 
 let colors = ['orange', 'purple', 'pink', 'yellow', 'green', 'darkblue', 'violet', 'red'];
+let firstLetters = [];
 
-/**
- * renders the sorted contacts array into the contact list 
- */
-function renderContacts () {
+function init() {
+    getFirstLetters();
+    renderContactList();
+}
+
+function renderContactList() {
     let contactList = document.getElementById('contacts');
     contactList.innerHTML = '';
-    sortContactsByName();
-    for (let i = 0; i < contacts.length; i++) {
-        const contact = contacts[i];
-        contactList.innerHTML += generateContactBoxHTML(contact, i);
+    for (let i = 0; i < firstLetters.length; i++) {
+        const firstLetter = firstLetters[i];
+        contactList.innerHTML += /*html*/`
+            <div class="first-letter-field" id="box${firstLetter}">
+                ${firstLetter}
+            </div>
+            ${renderContactBoxes(firstLetter)}
+        `;
     }
 }
 
-/**
- * 
- * @param {*} contact 
- * @param {*} index 
- * @returns 
- */
+function renderContactBoxes(firstLetter) {
+    let html = '';
+    for (let i = 0; i < contacts.length; i++) {
+        const contact = contacts[i];
+
+        if (contact['name'].toLocaleUpperCase().charAt(0) == firstLetter) {
+            html += generateContactBoxHTML(contact, i);
+        }
+    }
+    return html;
+}
+
 function generateContactBoxHTML(contact, index) {
     return /*html*/`
-        <div class=contact-box id="contactBox${index}">
+        <div class=contact-box id="contactBox${index}" onclick="showContact(${index}); changeContactToActive('contactBox${index}')">
                 <div class="initials ${contact['color']}">
                     <span>${getInitials(contact['name'])}</span>
                 </div>
@@ -58,6 +81,52 @@ function generateContactBoxHTML(contact, index) {
                 </div>
             </div>
     `;
+}
+
+function showContact(index) {
+    let contactCard = document.getElementById('contactCard');
+    let contact = contacts[index];
+    contactCard.innerHTML = generateContactCardHTML(contact);
+}
+
+function generateContactCardHTML(contact) {
+    return /*html*/`
+        <div class="contact-heading">
+            <div class="name-initials ${contact['color']}" id="nameInitials">${getInitials(contact['name'])}</div>
+            <div class="contact-heading-content">
+                <span class="cotact-name" id="contactName">${capitalizeFirstLetter(contact['name'])}</span>
+                    <div class="edit-contact">
+                        <div id="editContact"><img src="assets/img/contacts_edit.svg" alt="edit icon"> Edit</div>
+                        <div id="deleteContact"><img src="assets/img/contacts_delete.svg" alt="delete icon"> Delete</div>
+                    </div>
+            </div>
+        </div>
+
+        <span class="contact-information">Contact Information</span>
+
+        <div class="contact-info-box">
+            <div class="info-box">
+                <span class="bold">Email</span>
+                <span class="contact-email" id="contactEmail">${contact['email']}</span>
+            </div>
+
+            <div class="info-box">
+                <span class="bold">Phone</span>
+                <span>${contact['phone']}</span>
+            </div>
+        </div>
+    `
+}
+
+function getFirstLetters() {
+    firstLetters = [];
+    sortContactsByName();
+    contacts.forEach(contact => {
+        let firstLetter = contact.name.charAt(0).toUpperCase();
+        if (!firstLetters.includes(firstLetter)) {
+            firstLetters.push(firstLetter);
+        }
+    })
 }
 
 function getInitials(name) {
@@ -91,4 +160,18 @@ function sortContactsByName () {
         }
         return 0;
     })
+}
+
+function changeContactToActive(id) {
+    let activeContact = document.getElementById(id);
+    let contacts = document.getElementsByClassName('contact-box');
+    for (let i = 0; i < contacts.length; i++) {
+        const contact = contacts[i];
+        contact.classList.remove('active-contact');
+    }
+    activeContact.classList.add('active-contact');
+}
+
+function togglePopup() {
+    document.getElementById('popup').classList.toggle('d-none');
 }
