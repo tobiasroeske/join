b9e036d5d37d80ca95b236b2ef896e0af464c9f6
Tobias Roeske <122990092+tobiasroeske@users.noreply.github.com>
2024-02-24 10:44:03 +0100

    add Task implemented

--- js/addTask.js
+++ js/addTask.js
@@ -1,4 +1,21 @@
+let contacts = [];
 
+let tasks = [];
+let currentTask;
+
+async function init() {
+    includeHTML();
+    await loadContacts();
+    renderContacts();
+}
+
+async function loadContacts() {
+    try {
+        contacts = JSON.parse(await getItem('contacts'));
+    } catch (error) {
+        console.error('Loading error:', error);
+    }
+}
 
 function selectPriority(btnId, btnClass) {
     let allBtns = document.querySelectorAll('.prio-btn');
@@ -11,13 +28,48 @@ function selectPriority(btnId, btnClass) {
     });
 }
 
-function toggleCategories() {
-    document.getElementById('categoryPopup').classList.toggle('d-none');
+function renderContacts() {
+    let contactList = document.getElementById('contactList');
+    contactList.innerHTML = '';
+    for (let i = 0; i < contacts.length; i++) {
+        const contact = contacts[i];
+        contactList.innerHTML += /*html*/`
+            <li id="contact${i}" onclick="addContactToList(${i})">${contact['name']}</li>
+        `;
+    }
+}
+
+async function createNewTask() {
+    let titleInput = document.getElementById('titleInput');
+    let descriptionInput = document.getElementById('descriptionTextarea');
+    let contactInput = document.getElementById('assignedContacts');
+    let dateInput = document.getElementById('dateInput');
+    let categoryInput = document.getElementById('categoryInput');
+    currentTask = {
+        title: titleInput.value,
+        description: descriptionInput.value,
+        assignedContact: contactInput.value,
+        dueDate: dateInput.value,
+        category: categoryInput.value
+    };
+    tasks.push(currentTask);
+    await setItem('tasks', JSON.stringify('tasks'));
+}
+
+function addContactToList(index) {
+    let contactInputField = document.getElementById('assignedContacts')
+    contactInputField.value = contacts[index]['name'];
+    togglePopup('contactsPopup')
+}
+
+function togglePopup(id) {
+    document.getElementById(id).classList.toggle('d-none');
 }
 
 function addCategory(id) {
     let inputField = document.getElementById('categoryInput');
     let listItem = document.getElementById(id);
     inputField.value = listItem.innerHTML;
-    toggleCategories();
+    togglePopup('categoryPopup');
 }
+
