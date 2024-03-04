93d4d7b839d360a4cf5521d0fb9caa15c85cd2d9
Tobias Roeske <122990092+tobiasroeske@users.noreply.github.com>
2024-03-04 11:30:02 +0100

    summary responsive

--- js/summary.js
+++ js/summary.js
@@ -1,8 +1,15 @@
 let currentUser;
 let tasks = [];
+let todos = [];
+let inProgress = [];
+let awaitingFeedback = [];
+let done = [];
+let urgent = [];
 async function init() {
     await includeHTML()
     await loadCurrentUser();
+    sortTasks();
+    displayTasks();
 }
 
 async function loadCurrentUser() {
@@ -12,4 +19,30 @@ async function loadCurrentUser() {
     } catch (error) {
         console.error('Loading error:' + error);
     }
+}
+
+function sortTasks() {
+    tasks.forEach(task => {
+        if (task['currentColumn'] == 'toDo') {
+            todos.push(task);
+        } else if (task['currentColumn'] == 'inProgress') {
+            inProgress.push(task);
+        } else if (task['currentColumn'] == 'awaitingFeedback') {
+            awaitingFeedback.push(task);
+        } else if (task['currentColumn'] == 'done') {
+            done.push(task);
+        }
+        if (task['currentPriority'][0] == 'urgent') {
+            urgent.push(task);
+        }
+    })
+}
+
+function displayTasks() {
+    document.getElementById('tasksTodo').innerHTML = todos.length;
+    document.getElementById('tasksDone').innerHTML = done.length;
+    document.getElementById('tasksUrgent').innerHTML = urgent.length;
+    document.getElementById('allTasks').innerHTML = tasks.length;
+    document.getElementById('tasksInProgress').innerHTML = inProgress.length;
+    document.getElementById('tasksAwaitingFeedback').innerHTML = awaitingFeedback.length;
 }
